c008b48bdd455654ad8914e4419136fd1cd978ae
u-verma <er.umeshverma@gmail.com>
2020-05-25 15:46:03 +0200

    Shell Sort

--- src/main/java/com/org/easysolution/sorting/QuickSort.java
+++ src/main/java/com/org/easysolution/sorting/QuickSort.java
@@ -6,11 +6,8 @@ import static com.org.easysolution.util.CommonUtil.swapIndexValue;
 public class QuickSort {
 
     public static void main(String[] args) {
-
         int[] unsorted = {6, 3, 7, 2, 10, 4, 5, 0, 12, 6, 1, 8, 9, 3, 11};
-
         new QuickSort().quickSort(unsorted, 0, unsorted.length - 1);
-
     }
 
     public int partition(int[] elementsToSort, int lowerBoundIndex, int upperBoundIndex) {
---
+++ src/main/java/com/org/easysolution/sorting/ShellSort.java
@@ -0,0 +1,29 @@
+package com.org.easysolution.sorting;
+
+import static com.org.easysolution.util.CommonUtil.printElement;
+import static com.org.easysolution.util.CommonUtil.swapIndexValue;
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] unsorted = {6, 3, 7, 2, 10, 4, 5, 0, 12, 6, 1, 8, 9, 3, 11};
+        new ShellSort().sort(unsorted);
+    }
+
+    private void sort(int[] unsorted) {
+        int arrayLength = unsorted.length;
+        int gapSequence = 4;
+        while (gapSequence > 0) {
+            for (int currentIndex = 0; currentIndex < arrayLength; currentIndex++) {
+                int swapIndex = currentIndex + gapSequence;
+                if (swapIndex >= arrayLength) {
+                    break;
+                } else if (unsorted[currentIndex] > unsorted[swapIndex]) {
+                    swapIndexValue(unsorted, currentIndex, swapIndex);
+                }
+            }
+            gapSequence--;
+        }
+        printElement(unsorted);
+    }
+}
