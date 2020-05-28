fc38c2fd765cb74e2b3fdbb5d44f82958de08f4b
u-verma <er.umeshverma@gmail.com>
2020-05-28 09:54:15 +0200

    Balancing Bracket

---
+++ src/main/java/com/org/easysolution/challenge/BalancedBracket.java
@@ -0,0 +1,39 @@
+package com.org.easysolution.challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class BalancedBracket {
+    public static void main(String[] args) {
+        String sequence = "{{[[(())]]}}{";
+
+        Map bracketPairs = new HashMap<Character, Character>();
+        bracketPairs.put(')', '(');
+        bracketPairs.put('}', '{');
+        bracketPairs.put(']', '[');
+
+        boolean isBalanced = checkIfBalanced(sequence, bracketPairs);
+        System.out.println("check if bracket is balanced : " + isBalanced);
+    }
+
+    private static boolean checkIfBalanced(String sequence, Map<Character, Character> bracketPairs) {
+
+        Stack bracketStore = new Stack<Character>();
+
+        for (Character bracket : sequence.toCharArray()) {
+            if (bracketStore.empty() && bracketPairs.containsKey(bracket)) {
+                return false;
+            } else if (bracketPairs.containsKey(bracket) && bracketPairs.get(bracket)!=(bracketStore.pop())) {
+                return false;
+            } else if(!bracketPairs.containsKey(bracket)) {
+                bracketStore.push(bracket);
+            }
+        }
+
+        if (!bracketStore.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
